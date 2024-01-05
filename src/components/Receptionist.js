1ae5d679d8b7c11107925338763b9df3b8ef8144
Ramosh99 <einfetch@gmail.com>
2024-01-05 16:44:11 +0530

    checking

---
+++ src/components/Receptionist.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './rcomponents/Header'
+import Form from './rcomponents/From'
+
+
+export default function Receptionist() {
+  return (
+    <div>
+       <Header></Header>
+       <Form></Form>
+      
+    </div>
+  )
+}
