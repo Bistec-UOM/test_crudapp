ce55f52601d6155eab7984c4c4dbf62a1f5c22b8
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2024-01-06 22:51:39 +0530

    receptionit appointment list,edit appointment feature added

--- front end/src/components/Receptionist.js
+++ front end/src/components/Receptionist.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import Header from './rcomponents/Header'
 import Form from './rcomponents/From'
+import { useState } from 'react'
+import AppointmentList from './rcomponents/AppointmentList'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import EditPage from './rcomponents/EditPage'
+import dayjs from 'dayjs'
 
 
 export default function Receptionist() {
+  const [alist,setAList]=useState([])
+  const [count,setCount]=useState(0)
+
+    const [name,setName]=useState("")
+    const [address,setAddress]=useState("")
+    const [nic,setNic]=useState("")
+
+    const [nameError,setNameError]=useState(false)
+    const [addressError,setAddressError]=useState(false)
+    const [nicError,setNicError]=useState(false)
+    const [timevalueError,setTimeValueError]=useState(false)
+   
+
+  
+   
+    const [timevalue, setTimeValue] = useState(dayjs('2022-04-17T15:30'));
+
+
+
   return (
     <div>
        <Header></Header>
-       <Form></Form>
+       <Form name={name} setName={setName} address={address} setAddress={setAddress} nic={nic} setNic={setNic} timevalue={timevalue} setTimeValue={setTimeValue} alist={alist} setAList={setAList} count={count} setCount={setCount} nameError={nameError} setNameError={setNameError} addressError={addressError} setAddressError={setAddressError} nicError={nicError} setNicError={setNicError} timevalueError={timevalueError} setTimeValueError={setTimeValueError}></Form>
+       <AppointmentList  name={name} setName={setName} address={address} setAddress={setAddress} nic={nic} setNic={setNic} timevalue={timevalue} setTimeValue={setTimeValue} alist={alist} setAList={setAList} count={count} setCount={setCount} nameError={nameError} setNameError={setNameError} addressError={addressError} setAddressError={setAddressError} nicError={nicError} setNicError={setNicError} timevalueError={timevalueError} setTimeValueError={setTimeValueError}></AppointmentList>
+       {/* <BrowserRouter>
+        <Routes>
+          <Route element={<EditPage name={name} setName={setName} address={address} setAddress={setAddress} nic={nic} setNic={setNic} timevalue={timevalue} setTimeValue={setTimeValue} alist={alist} setAList={setAList} count={count} setCount={setCount} nameError={nameError} setNameError={setNameError} addressError={addressError} setAddressError={setAddressError} nicError={nicError} setNicError={setNicError} timevalueError={timevalueError} setTimeValueError={setTimeValueError}/>}></Route>
+        </Routes>
+       </BrowserRouter> */}
+      
       
     </div>
   )
