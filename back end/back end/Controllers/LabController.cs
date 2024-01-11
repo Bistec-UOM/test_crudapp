cc9bc386147dc1b51e683f48b2147294dde67a6f
Ramosh99 <einfetch@gmail.com>
2024-01-11 21:30:58 +0530

    add another controller

--- back end/back end/Controllers/LabController.cs
+++ back end/back end/Controllers/LabController.cs
@@ -18,60 +18,6 @@ namespace back_end.Controllers
 
 
 
-    //for admin things
-    [Route("api/[controller]")]
-    [ApiController]
-    public class Admin_DataAPIController : ControllerBase
-    {
-        private readonly ApplicationDbContext _db;
-        public Admin_DataAPIController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-        [HttpGet]
-        public ActionResult<IEnumerable<Admin_DataDto>> GetData()
-        {
-            return Ok(_db.admin_Datas);
-        }
-        [HttpPost]
-        public ActionResult<Admin_DataDto> CreateAdmin([FromBody] Admin_DataDto NDTO)
-        {
-            //to create DTo after thing
-            Admin_Data model = new()
-            {
-                Id = NDTO.Id,
-                firstName = NDTO.firstName,
-                lastName = NDTO.lastName,
-                email = NDTO.email,
-                phnNumber = NDTO.phnNumber,
-                dateOfBirth = NDTO.dateOfBirth,
-                password = NDTO.password
-            };
-            _db.admin_Datas.Add(model);
-            _db.SaveChanges();
-
-            return Ok(NDTO);
-        }
-        [HttpDelete("{id:int}")]
-        public IActionResult DeleteAdmin(int id)
-        {
-
-            var dts = _db.admin_Datas.FirstOrDefault(u => u.Id == id);
-
-            if (dts != null)
-            {
-                _db.admin_Datas.Remove(dts);
-                _db.SaveChanges();
-                return NoContent();
-                _db.admin_Datas.Remove(dts);
-            }
-            else
-            {
-            }
-            _db.SaveChanges();
-            return NoContent();
-        }
-    }
     //end of admin
 
 
