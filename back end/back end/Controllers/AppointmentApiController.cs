a7b5fb9d098f8d172bb8df552a815d8dcecfd6d6
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2024-01-12 21:20:25 +0530

    receptionist backend functionlites create,fetch,delete added

---
+++ back end/back end/Controllers/AppointmentApiController.cs
@@ -0,0 +1,53 @@
+﻿
+using back_end.Data;
+using back_end.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_end.Controllers
+{
+    [Route("api/appointmentapi")]
+
+    [ApiController]
+    public class AppointmentApiController : ControllerBase
+    {
+        private readonly AppointmentDbContext _db;
+        public AppointmentApiController(AppointmentDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Appointment>> GetAppointments()
+        {
+            return Ok(_db.appointments.ToList());
+        }
+        [HttpPost]
+
+        public ActionResult<Appointment> CreateAppointment([FromBody] Appointment appointment)
+        {
+            if (appointment == null)
+            {
+                return BadRequest("Invalid appointment details.pleasee provide valid appointment information");
+            }
+            _db.Add(appointment);
+            _db.SaveChanges();
+            return Ok(appointment);
+
+
+
+        }
+        [HttpDelete("{id}")]
+        public ActionResult DeleteAppointment(int id)
+        {
+            Appointment model=_db.appointments.FirstOrDefault(x => x.Id == id);
+            if(model == null)
+            {
+                return BadRequest();
+            }
+            _db.appointments.Remove(model);
+            _db.SaveChanges();
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ back end/back end/Models/Appointment.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace back_end.Models
+{
+    public class Appointment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string nic { get; set; }
+        public string address { get; set; }
+        public DateTime? time { get; set; }
+    }
+}
