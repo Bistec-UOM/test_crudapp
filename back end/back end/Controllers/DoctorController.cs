424ac9caa9a8d378a440662e5068f364811c24a8
nethmi <nethmieranga20@gmail.com>
2024-01-18 18:41:12 +0530

    Controller get and post

--- back end/back end/Controllers/DoctorController.cs
+++ back end/back end/Controllers/DoctorController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using back_end.Data;
+using back_end.Models.Dto;
+using back_end.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace back_end.Controllers
 {
@@ -7,6 +11,49 @@ namespace back_end.Controllers
     [ApiController]
     public class DoctorController : ControllerBase
     {
+        private readonly DoctorDBContext _DoctorContext;
 
+        public DoctorController(DoctorDBContext DoctorContext)
+        {
+            _DoctorContext = DoctorContext;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<Doctor>>> Getpatients()
+        {
+            if (_DoctorContext.patients== null)
+            {
+                return NotFound();
+            }
+            return await _DoctorContext.patients.ToListAsync();
+
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Doctor>> Getpatient(int id)
+        {
+            if (_DoctorContext.patients == null)
+            {
+                return NotFound();
+            }
+            var employee = await _DoctorContext.patients.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return employee;
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<Doctor>> Postpatient(Doctor x)
+        {
+           _DoctorContext.patients.Add(x);
+            await _DoctorContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Getpatient), new { id = x.ID }, x);
+        }
     }
 }
