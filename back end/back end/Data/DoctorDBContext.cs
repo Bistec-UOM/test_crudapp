053cab8f14f045fa41da1d378e72d02667e0cffc
nethmi <nethmieranga20@gmail.com>
2024-01-18 16:37:49 +0530

    Doctor model and DBContex created

---
+++ back end/back end/Data/DoctorDBContext.cs
@@ -0,0 +1,15 @@
+﻿using back_end.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace back_end.Data
+{
+    public class DoctorDBContext : DbContext
+    {
+        public DoctorDBContext(DbContextOptions<DoctorDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Doctor> patients { get; set; }
+
+    }
+}
